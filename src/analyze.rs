eec1ebc68f9dc8ddd858f77d431f0ca17b075247
Ernesto Ramírez <ernestorb_@outlook.com>
2024-10-08 20:59:19 -0600

    Convertir a flotante valor entero en asignación

--- src/analyze.rs
+++ src/analyze.rs
@@ -320,8 +320,14 @@ impl Analyzer {
                                                 "La asignación de un flotante a un entero no es válida".to_string(), cursor: exp_cursor.clone().unwrap_or(fake_cursor()) }
                                         );
                                     } else {
-                                        symbol.value = val.clone();
-                                        *exp_value = val.clone();
+                                        if let ExpType::Float = typ {
+                                            symbol.value = val.clone().map(|v| v.to_float()).flatten();
+                                            *exp_value = val.clone().map(|v| v.to_float()).flatten();
+                                        } else {
+                                            symbol.value = val.clone();
+                                            *exp_value = val.clone();
+                                        }
+                                       
                                     }
                                 
                                 }
